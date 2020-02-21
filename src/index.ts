bc02b3e6e065f6d749c68c72df54dd187a0aa349
ywarezk <ywarezk@gmail.com>
2020-02-21 18:35:37 +0200

    added eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -9,32 +9,25 @@
  */
 
 import 'zone.js';
-import {
-  Request, Response, NextFunction, RequestHandler,
-} from 'express';
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-function onHandleError(
-  _parentZoneDelegate: ZoneDelegate,
-  currentZone: Zone,
-  _targetZone: Zone,
-  error: Error,
-): boolean {
-  const next = currentZone.get('next');
-  next(error);
-  return false;
+function onHandleError(_parentZoneDelegate: ZoneDelegate, currentZone: Zone, _targetZone: Zone, error: Error): boolean {
+    const next = currentZone.get('next');
+    next(error);
+    return false;
 }
 
 /**
  * Openning a zone for every req and saving the req/res object in the zone
  */
 export default function zoneErrors(): RequestHandler {
-  return (req: Request, _res: Response, next: NextFunction): void => {
-    Zone.current.fork({
-      properties: {
-        next,
-      },
-      name: 'express-zone',
-      onHandleError,
-    });
-  };
+    return (req: Request, _res: Response, next: NextFunction): void => {
+        Zone.current.fork({
+            properties: {
+                next,
+            },
+            name: 'express-zone',
+            onHandleError,
+        });
+    };
 }
