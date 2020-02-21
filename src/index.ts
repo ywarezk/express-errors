feb0b27de5f78db91256e4a2bbf2aa858a76df26
ywarezk <ywarezk@gmail.com>
2020-02-21 14:06:20 +0200

    finished modifying lint for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
 /**
-<<<<<<< HEAD
- * The middleware main logic will be here
- * 
- * Created February 15th, 2020
- * @author: ywarezk
- * @version: 1.0.0
- * @copyright: Nerdeez Ltd
- */
-
-import "zone.js";
-=======
  * Entry point file of the middleware
  *
  * Created February 20th, 2020
@@ -19,13 +8,33 @@ import "zone.js";
  * @license: MIT
  */
 
+import 'zone.js';
 import {
   Request, Response, NextFunction, RequestHandler,
 } from 'express';
 
-export default function zoneErrors(): RequestHandler {
-  return (req: Request, res: Response, next: NextFunction): void => {
+function onHandleError(
+  _parentZoneDelegate: ZoneDelegate,
+  currentZone: Zone,
+  _targetZone: Zone,
+  error: Error,
+): boolean {
+  const next = currentZone.get('next');
+  next(error);
+  return false;
+}
 
+/**
+ * Openning a zone for every req and saving the req/res object in the zone
+ */
+export default function zoneErrors(): RequestHandler {
+  return (req: Request, _res: Response, next: NextFunction): void => {
+    Zone.current.fork({
+      properties: {
+        next,
+      },
+      name: 'express-zone',
+      onHandleError,
+    });
   };
 }
->>>>>>> daf465a7003a1ac2b64925260a1535eb951ed4f3
