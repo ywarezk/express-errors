16237471f6ee7e9b60a1a2fa71948999d66d4bc7
ywarezk <ywarezk@gmail.com>
2020-03-09 13:57:21 +0200

    added the strategy pattern adding a test for that

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,51 @@
 import 'zone.js';
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 
+/**
+ * we attach this function to the zone error handling
+ * the job of this function is to pass the error to express error middleware.
+ *
+ * @param _parentZoneDelegate
+ * @param currentZone
+ * @param _targetZone
+ * @param error
+ */
 function onHandleError(_parentZoneDelegate: ZoneDelegate, currentZone: Zone, _targetZone: Zone, error: Error): boolean {
     const next = currentZone.get('next');
+    const strategy: ErrorStrategy = currentZone.get('strategy');
+    if (strategy) {
+        const req = currentZone.get('req');
+        strategy.handleError(error, req);
+    }
     next(error);
     return false;
 }
 
 /**
+ * This interface represents a strategy for dealing with the errors
+ * Strategy can print the error, save it to the db, etc.
+ */
+export interface ErrorStrategy {
+    handleError: (err: Error, req: Request) => Promise<void>;
+}
+
+/**
+ * This is the configurations that the middleware is getting
+ */
+export interface ZoneErrorsConfig {
+    strategy?: ErrorStrategy;
+}
+
+/**
  * Openning a zone for every req and saving the req/res object in the zone
  */
-export default function zoneErrors(): RequestHandler {
-    return (_req: Request, _res: Response, next: NextFunction): void => {
+export default function zoneErrors(config: ZoneErrorsConfig = {}): RequestHandler {
+    return (req: Request, _res: Response, next: NextFunction): void => {
         const expressZone = Zone.current.fork({
             properties: {
                 next,
+                req,
+                strategy: config.strategy,
             },
             name: 'express-zone',
             onHandleError,
